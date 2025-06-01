fdc56a1b8484962b6df6d4b52e34e8dd11cf9846
boomNDS <boomsixboom@gmail.com>
2025-06-02 05:10:55 +0700

    fix: remove userId form comment dto

--- src/comments/dto/create-comment.dto.ts
+++ src/comments/dto/create-comment.dto.ts
@@ -1,30 +1,25 @@
-import { ApiProperty } from "@nestjs/swagger"
+import { ApiProperty } from "@nestjs/swagger";
 import {
   IsNotEmpty,
   IsNumberString,
   IsString,
   IsOptional,
   IsNumber,
-} from "class-validator"
+} from "class-validator";
 
 export class CreateCommentDto {
   @ApiProperty({ example: "This is a comment." })
   @IsNotEmpty()
   @IsString()
-  content: string
-
-  @ApiProperty({ example: "1" })
-  @IsNotEmpty()
-  @IsNumberString()
-  userId: string
+  content: string;
 
   @ApiProperty({ example: "2" })
   @IsNotEmpty()
   @IsNumber()
-  postId: number
+  postId: number;
 
   @ApiProperty({ example: "", required: false })
   @IsOptional()
   @IsNumberString()
-  parentId?: string
+  parentId?: string;
 }
